048a2199a449fd0785305ed804bfe14da9a241f9
Muni Kumar <mkpvasu@uw.edu>
2023-06-05 17:54:24 -0700

    Updated training.py to include validation set and plot the losses with epochs

--- idc_with_vae_and_dcgan/training.py
+++ idc_with_vae_and_dcgan/training.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.optim as optim
 import torchvision.utils
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
@@ -19,7 +19,7 @@ from discriminator import Discriminator
 from vae_encoder import VanillaVAEEncoder
 
 device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 class CustomImageDataset(Dataset):
     def __init__(self, path, pattern, transform=None):
@@ -61,7 +61,7 @@ class CustomImageDataset(Dataset):
 
 class Data:
     def __init__(self):
-        self.dataroot = os.path.join(os.getcwd(), "data", "dcgan_data")
+        self.dataroot = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "dcgan_data")
         self.pattern = "d_*.npz"
         self.image_size = 64
         self.batch_size = 16
@@ -75,13 +75,18 @@ class Data:
 
     def dataset_prep(self):
         dataset = CustomImageDataset(path=self.dataroot, pattern=self.pattern, transform=self.transform)
-        dataloader = DataLoader(dataset=dataset, batch_size=self.batch_size, shuffle=self.shuffle,
-                                num_workers=self.num_workers)
-        return dataloader
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+        train_dataloader = DataLoader(dataset=train_dataset, batch_size=self.batch_size, shuffle=self.shuffle,
+                                      num_workers=self.num_workers)
+        val_dataloader = DataLoader(dataset=val_dataset, batch_size=self.batch_size, shuffle=False,
+                                    num_workers=self.num_workers)
+        return train_dataloader, val_dataloader
 
 
 class Training:
-    def __init__(self, n_epochs: int = 10):
+    def __init__(self, n_epochs: int = 2):
         # set random seed for reproducibility
         self.manualSeed = 1000
         # manualSeed = random.randint(1, 10000) # use if you want new results
@@ -153,118 +158,152 @@ class Training:
         vae_optimizer = self.vae_optimizer(vae_encoder.parameters(), lr=self.vae_lr, betas=self.betas)
         gen_optimizer = self.gen_optimizer(generator.parameters(), lr=self.gen_lr, betas=self.betas)
         dis_optimizer = self.dis_optimizer(discriminator.parameters(), lr=self.dis_lr, betas=self.betas)
-        dataloader = Data().dataset_prep()
+        train_dataloader, val_dataloader = Data().dataset_prep()
 
         # Lists to keep track of progress
         img_list = []
+        val_img_list = []
         gen_losses = []
         dis_losses = []
+        val_gen_losses = []
+        val_dis_losses = []
         iters = 0
 
         print("Starting Training Loop...")
         # For each epoch
         for epoch in range(self.n_epochs):
+            gen_loss, dis_loss, val_gen_loss, val_dis_loss, train_batches, val_batches = 0, 0, 0, 0, 0, 0
             # For each batch in the dataloader
-            for i, data in enumerate(dataloader, 0):
+            for mode in ["train", "val"]:
+                if mode == "train":
+                    loader = train_dataloader
+                else:
+                    loader = val_dataloader
+                    torch.no_grad()
 
-                ############################
-                # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-                ###########################
-                # Train with all-real batch
-                discriminator.zero_grad()
-                # Format batch
-                real_cpu = data["delta_vmap"].to(device)
-                b_size = real_cpu.size(0)
-                label = torch.full((b_size,), self.labels["real"], dtype=torch.float, device=device)
-                # Forward pass real batch through D
-                output = discriminator(real_cpu).view(-1)
-                # Calculate loss on all-real batch
-                dis_error_real = criterion(output, label)
-                # Calculate gradients for D in backward pass
-                dis_error_real.backward()
-                D_x = output.mean().item()
+                for i, data in enumerate(loader, 0):
 
-                # Train with all-fake batch
-                # Generate batch of latent vectors
-                combined_map = data["combined_map"]
-                vae_latent_embedding = vae_encoder.forward(combined_map).to(self.device)
-                vae_latent_embedding = nn.functional.normalize(vae_latent_embedding)
-                di = data["dI"].to(self.device)
-                vae_latent_embedding_with_di = torch.cat((vae_latent_embedding, di), dim=1).to(self.device)
-                vae_latent_embed_vector = torch.reshape(vae_latent_embedding_with_di,
-                                                        (b_size, self.total_latent_dims, 1, 1))
+                    ############################
+                    # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+                    ###########################
+                    # Train with all-real batch
+                    discriminator.zero_grad()
+                    # Format batch
+                    real_cpu = data["delta_vmap"].to(device)
+                    b_size = real_cpu.size(0)
+                    label = torch.full((b_size,), self.labels["real"], dtype=torch.float, device=device)
+                    # Forward pass real batch through D
+                    output = discriminator(real_cpu).view(-1)
+                    # Calculate loss on all-real batch
+                    dis_error_real = criterion(output, label)
+                    if mode == "train":
+                        # Calculate gradients for D in backward pass
+                        dis_error_real.backward()
+                    D_x = output.mean().item()
 
-                # Generate fake image batch with G
-                fake = generator(vae_latent_embed_vector)
-                label.fill_(self.labels["fake"])
-                # Classify all fake batch with D
-                output = discriminator(fake.detach()).view(-1)
-                # Calculate D's loss on the all-fake batch
-                dis_error_fake = criterion(output, label)
-                # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-                dis_error_fake.backward()
-                D_G_z1 = output.mean().item()
-                # Compute error of D as sum over the fake and the real batches
-                dis_error = dis_error_real + dis_error_fake
-                # Update D
-                dis_optimizer.step()
+                    # Train with all-fake batch
+                    # Generate batch of latent vectors
+                    combined_map = data["combined_map"]
+                    vae_latent_embedding = vae_encoder.forward(combined_map).to(self.device)
+                    vae_latent_embedding = nn.functional.normalize(vae_latent_embedding)
+                    di = data["dI"].to(self.device)
+                    vae_latent_embedding_with_di = torch.cat((vae_latent_embedding, di), dim=1).to(self.device)
+                    vae_latent_embed_vector = torch.reshape(vae_latent_embedding_with_di,
+                                                            (b_size, self.total_latent_dims, 1, 1))
 
-                ############################
-                # (2) Update G network: maximize log(D(G(z)))
-                ###########################
-                generator.zero_grad()
-                label.fill_(self.labels["real"])  # fake labels are real for generator cost
-                # Since we just updated D, perform another forward pass of all-fake batch through D
-                output = discriminator(fake).view(-1)
-                # Calculate G's loss based on this output
-                gen_error = criterion(output, label)
-                # Update VAE Encoder to generate better latent vectors
-                delta_vmaps = data["delta_vmap"].to(self.device)
-                # vae error between fake and delta_maps
-                softmax_fake = nn.functional.softmax(fake, dim=2)
-                softmax_d_vmaps = nn.functional.softmax(delta_vmaps, dim=2)
-                vae_error = vae_criterion(softmax_fake, softmax_d_vmaps)
-                # total error
-                total_error = gen_error + (- self.vae_loss_scaling_factor * vae_error)
-                # Calculate gradients for G
-                total_error.backward()
-                D_G_z2 = output.mean().item()
-                # Update G
-                gen_optimizer.step()
+                    # Generate fake image batch with G
+                    fake = generator(vae_latent_embed_vector)
+                    label.fill_(self.labels["fake"])
+                    # Classify all fake batch with D
+                    output = discriminator(fake.detach()).view(-1)
+                    # Calculate D's loss on the all-fake batch
+                    dis_error_fake = criterion(output, label)
+                    if mode == "train":
+                        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+                        dis_error_fake.backward()
+                    D_G_z1 = output.mean().item()
+                    # Compute error of D as sum over the fake and the real batches
+                    dis_error = dis_error_real + dis_error_fake
+                    if mode == "train":
+                        # Update D
+                        dis_optimizer.step()
 
-                # vae_error.backward(retain_graph=True)
-                # Update VAE
-                vae_optimizer.step()
+                    ############################
+                    # (2) Update G network: maximize log(D(G(z)))
+                    ###########################
+                    generator.zero_grad()
+                    label.fill_(self.labels["real"])  # fake labels are real for generator cost
+                    # Since we just updated D, perform another forward pass of all-fake batch through D
+                    output = discriminator(fake).view(-1)
+                    # Calculate G's loss based on this output
+                    gen_error = criterion(output, label)
+                    # Update VAE Encoder to generate better latent vectors
+                    delta_vmaps = data["delta_vmap"].to(self.device)
+                    # vae error between fake and delta_maps
+                    softmax_fake = nn.functional.softmax(fake, dim=2)
+                    softmax_d_vmaps = nn.functional.softmax(delta_vmaps, dim=2)
+                    vae_error = vae_criterion(softmax_fake, softmax_d_vmaps)
+                    # total error
+                    total_error = gen_error + (- self.vae_loss_scaling_factor * vae_error)
+                    D_G_z2 = output.mean().item()
+                    if mode == "train":
+                        # Calculate gradients for G
+                        total_error.backward()
+                        # Update G
+                        gen_optimizer.step()
+                        # vae_error.backward(retain_graph=True)
+                        # Update VAE
+                        vae_optimizer.step()
 
-                # Output training stats
-                if i % 50 == 0:
                     # Output training stats
                     if i % 50 == 0:
-                        print(f"[{epoch}/{self.n_epochs}][{i}/{len(dataloader)}]\tVAE Loss: {vae_error.item():.4f}"
-                              f"\tLoss_D: {dis_error.item():.4f} \tLoss_G: {gen_error.item():.4f}\tD(x): {D_x:.4f}"
-                              f"\tD(G(z)): {D_G_z1:.4f} / {D_G_z2:.4f}")
+                        # Output training stats
+                        if i % 50 == 0:
+                            print(f"[{epoch}/{self.n_epochs}][{i}/{len(loader)}]\tVAE Loss: {vae_error.item():.4f}"
+                                  f"\tLoss_D: {dis_error.item():.4f} \tLoss_G: {gen_error.item():.4f}\tD(x): {D_x:.4f}"
+                                  f"\tD(G(z)): {D_G_z1:.4f} / {D_G_z2:.4f}")
 
-                # Save Losses for plotting later
-                gen_losses.append(gen_error.item())
-                dis_losses.append(dis_error.item())
+                    if mode == "train":
+                        gen_loss += gen_error.item()
+                        dis_loss += dis_error.item()
+                        train_batches += 1
 
-                # Check how the generator is doing by saving G's output on fixed_noise
-                if (iters % 500 == 0) or ((epoch == self.n_epochs - 1) and (i == len(dataloader) - 1)):
-                    with torch.no_grad():
-                        fake = generator(fixed_noise).detach().cpu()
-                    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                        # Check how the generator is doing by saving G's output on fixed_noise
+                        if (iters % 500 == 0) or ((epoch == self.n_epochs - 1) and (i == len(train_dataloader) - 1)):
+                            with torch.no_grad():
+                                fake = generator(fixed_noise).detach().cpu()
+                            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                    else:
+                        val_gen_loss += gen_error.item()
+                        val_dis_loss += dis_error.item()
+                        val_batches += 1
+
+                        # Check how the generator is doing by saving G's output on fixed_noise
+                        if (iters % 500 == 0) or ((epoch == self.n_epochs - 1) and (i == len(loader) - 1)):
+                            fake = generator(fixed_noise).detach().cpu()
+                            val_img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
                 iters += 1
 
-        return img_list, gen_losses, dis_losses
+                if mode == "train":
+                    # Save Losses for plotting later
+                    gen_losses.append(gen_loss/train_batches)
+                    dis_losses.append(dis_loss/train_batches)
+                else:
+                    # Save Losses for plotting later
+                    val_gen_losses.append(val_gen_loss/val_batches)
+                    val_dis_losses.append(val_dis_loss/val_batches)
+
+        return img_list, val_img_list, gen_losses, dis_losses, val_gen_losses, val_dis_losses
 
     def execute(self):
         return self.training()
 
 
-class Visualize_Model:
+class VisualizeModel:
     def __init__(self):
-        self.gen_imgs, self.gen_losses, self.dis_losses = Training(n_epochs=10).execute()
+        self.gen_imgs, self.val_gen_imgs, self.gen_losses, self.val_dis_losses, \
+            self.val_gen_losses, self.dis_losses = Training(n_epochs=30).execute()
 
     def save_gen_images(self):
         path = os.path.join(os.getcwd(), "generated_images")
@@ -277,9 +316,11 @@ class Visualize_Model:
     def gen_dis_loss_training(self):
         plt.figure(figsize=(10, 5))
         plt.title("Generator and Discriminator Loss During Training")
-        plt.plot(self.gen_losses, label="G")
-        plt.plot(self.dis_losses, label="D")
-        plt.xlabel("iterations")
+        plt.plot(self.gen_losses, label="G - Train")
+        plt.plot(self.val_gen_losses, label="G - Validation", linestyle="--")
+        plt.plot(self.dis_losses, label="D - Train")
+        plt.plot(self.val_dis_losses, label="D - Validation", linestyle="--")
+        plt.xlabel("Epochs")
         plt.ylabel("Loss")
         plt.legend()
         plt.show()
@@ -297,7 +338,9 @@ class Visualize_Model:
 
 
 def main():
-    Visualize_Model().gen_output()
+    viz_model = VisualizeModel()
+    viz_model.gen_output()
+    viz_model.gen_dis_loss_training()
 
 
 if __name__ == "__main__":
